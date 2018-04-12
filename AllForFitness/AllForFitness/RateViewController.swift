f70cdd4828fd41cd634afe74abafab86d6c2535f
TimurSaidov <tim77777@inbox.ru>
2018-04-12 19:57:44 +0300

    Fixed bugs #3

--- AllForFitness/AllForFitness/RateViewController.swift
+++ AllForFitness/AllForFitness/RateViewController.swift
@@ -13,7 +13,12 @@ class RateViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Эффект нечеткости картинки imageView в RateViewController.
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.view.insertSubview(blurEffectView, at: 1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +26,5 @@ class RateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
